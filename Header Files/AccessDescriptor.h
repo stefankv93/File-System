5684773e41286b8215a5b6b404a5c1751d8c96a6
stefankv93 <stefankv93@gmail.com>
2014-09-16 12:56:35 +0200

    Added and implemented AccesssDescriptor class

---
+++ Header Files/AccessDescriptor.h
@@ -0,0 +1,36 @@
+//
+//
+//  @ Project : FileSystem
+//  @ File Name : AccessDescriptor.h
+//  @ Date : 9/15/2014
+//  @ Author : Stefan Djordjevic
+//
+//
+
+
+#ifndef _ACCESSDESCRIPTOR_H
+#define _ACCESSDESCRIPTOR_H
+
+#include <string>
+#include <vector>
+using namespace std;
+
+#include "IdentifiableObject.h"
+class FilesystemObject;
+
+class AccessDescriptor : public IdentifiableObject
+{
+private:
+	vector<string> allowedOperations;
+protected:
+	FilesystemObject *protectedObject;
+public:
+	AccessDescriptor();
+	void add(string operationName);
+	void remove(string operationName);
+	const vector<string>& getAllowedOperations() const;
+	bool checkAccess(string operationName);
+	
+};
+
+#endif  //_ACCESSDESCRIPTOR_H
---
+++ Source Files/AccessDescriptor.cpp
@@ -0,0 +1,55 @@
+//
+//
+//  @ Project : FileSystem
+//  @ File Name : AccessDescriptor.cpp
+//  @ Date : 9/15/2014
+//  @ Author : Stefan Djordjevic
+//
+//
+
+
+#include "../Header Files/AccessDescriptor.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+AccessDescriptor::AccessDescriptor():allowedOperations()
+{
+
+}
+
+void AccessDescriptor::add(string operationName)
+{
+	allowedOperations.push_back(operationName);
+}
+
+void AccessDescriptor::remove(string operationName)
+{
+	for(vector<string>::iterator it = allowedOperations.begin();
+		it != allowedOperations.end(); ++it)
+	{
+		if(!(*it).compare(operationName)) 
+		{
+			allowedOperations.erase(it);
+			break;
+		}
+	}
+
+}
+
+const vector<string>& AccessDescriptor::getAllowedOperations() const
+{
+	return allowedOperations;
+}
+
+bool AccessDescriptor::checkAccess(string operationName)
+{
+	for(vector<string>::iterator it = allowedOperations.begin();
+		it != allowedOperations.end(); it++)
+	{
+		if(!(*it).compare(operationName)) return true;
+	}
+
+	return false;
+}
+
