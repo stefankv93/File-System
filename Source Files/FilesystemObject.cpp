8c9163d41124c7561d4b40d7c7d0c3cd2afe48c9
stefankv93 <stefankv93@gmail.com>
2014-09-16 13:14:03 +0200

    Added and implemented FilesystemObject class

---
+++ Source Files/FilesystemObject.cpp
@@ -0,0 +1,41 @@
+//
+//
+//  @ Project : FileSystem
+//  @ File Name : FilesystemObject.cpp
+//  @ Date : 9/16/2014
+//  @ Author : Stefan Djordjevic
+//
+//
+
+
+#include "../Header Files/FilesystemObject.h"
+#include "../Header Files/AccessDescriptor.h"
+#include <string>
+using namespace std;
+
+FilesystemObject::FilesystemObject(string fName)
+{
+	name = fName;
+	accessDescriptor = new AccessDescriptor(this);
+}
+
+string FilesystemObject::getName()
+{
+	return name;
+}
+
+void FilesystemObject::grantAccess(string operationName)
+{
+	accessDescriptor->add(operationName);
+}
+
+void FilesystemObject::revokeAccess(string operationName)
+{
+	accessDescriptor->remove(operationName);
+}
+
+bool FilesystemObject::checkAccess(string operationName)
+{
+	return accessDescriptor->checkAccess(operationName);
+}
+
