bb10591502a774642ae5f0a2c9d8679ce59d65b8
stefankv93 <stefankv93@gmail.com>
2014-09-16 13:14:30 +0200

    Added and implemented FilesystemObject class

---
+++ Header Files/FilesystemObject.h
@@ -0,0 +1,36 @@
+//
+//
+//  @ Project : FileSystem
+//  @ File Name : FilesystemObject.h
+//  @ Date : 9/16/2014
+//  @ Author : Stefan Djordjevic
+//
+//
+
+
+#ifndef _FILESYSTEMOBJECT_H
+#define _FILESYSTEMOBJECT_H
+
+#include "IdentifiableObject.h"
+#include <string>
+using namespace std;
+
+class FilesystemVisitor;
+class AccessDescriptor;
+
+class FilesystemObject : public IdentifiableObject
+{
+private:
+	string name;
+protected:
+	AccessDescriptor *accessDescriptor;
+public:
+	FilesystemObject(string fName);
+	string getName();
+	virtual void accept(FilesystemVisitor v) = 0;
+	void grantAccess(string operationName);
+	void revokeAccess(string operationName);
+	bool checkAccess(string operationName);
+};
+
+#endif  //_FILESYSTEMOBJECT_H
