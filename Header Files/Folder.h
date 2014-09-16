3e7feffb8fad84de1af7d655edbe8edf74bd834f
stefankv93 <stefankv93@gmail.com>
2014-09-16 17:00:08 +0200

    Added and implemented Folder class

---
+++ Header Files/Folder.h
@@ -0,0 +1,32 @@
+//
+//
+//  @ Project : FileSystem
+//  @ File Name : Folder.h
+//  @ Date : 9/16/2014
+//  @ Author : Stefan Djordjevic
+//
+//
+
+
+#ifndef _FOLDER_H
+#define _FOLDER_H
+
+#include "FilesystemObject.h"
+#include <vector>
+#include <string>
+using namespace std;
+class FilesystemVisitor;
+
+class Folder : public FilesystemObject
+{
+private:
+	vector<FilesystemObject *> *containedObjects;
+public:
+	Folder(string name);
+	~Folder();
+	void accept(FilesystemVisitor *v);
+	void addFilesystemObject(FilesystemObject *o);
+	vector<FilesystemObject *>& getContainedObjects();
+};
+
+#endif  //_FOLDER_H
---
+++ Source Files/Folder.cpp
@@ -0,0 +1,48 @@
+//
+//
+//  @ Project : FileSystem
+//  @ File Name : Folder.cpp
+//  @ Date : 9/16/2014
+//  @ Author : Stefan Djordjevic
+//
+//
+
+
+#include "../Header Files/Folder.h"
+#include "../Header Files/FilesystemVisitor.h"
+#include "../Header Files/FilesystemObject.h"
+#include <vector>
+#include <string>
+using namespace std;
+
+Folder::Folder(string name):FilesystemObject(name)
+{
+	containedObjects = new vector<FilesystemObject *>();
+}
+
+Folder::~Folder()
+{
+	for(vector<FilesystemObject *>::iterator it = containedObjects->begin();
+		it != containedObjects->end(); it++)
+	{
+		delete (*it);
+	}
+
+	delete containedObjects;
+}
+
+void Folder::accept(FilesystemVisitor *v)
+{
+
+}
+
+void Folder::addFilesystemObject(FilesystemObject *o)
+{
+	containedObjects->push_back(o);
+}
+
+vector<FilesystemObject *>& Folder::getContainedObjects()
+{
+	return *containedObjects;
+}
+
