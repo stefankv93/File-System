5c410457ffd1d60b3404c650daf670ef98ab955e
stefankv93 <stefankv93@gmail.com>
2014-09-16 17:23:05 +0200

    Added and implemented FilesystemOperation class

---
+++ Header Files/FilesystemOperation.h
@@ -0,0 +1,34 @@
+//
+//
+//  @ Project : FileSystem
+//  @ File Name : FilesystemOperation.h
+//  @ Date : 9/16/2014
+//  @ Author : Stefan Djordjevic
+//
+//
+
+
+#ifndef _FILESYSTEMOPERATION_H
+#define _FILESYSTEMOPERATION_H
+
+#include <string>
+using namespace std;
+
+class FilesystemObject;
+
+class FilesystemOperation
+{
+private:
+	string opName;
+	FilesystemObject *objParameter;
+	bool checkPermissions();
+protected:
+	virtual void performOperation() = 0;
+public:
+	FilesystemOperation(string fOpName, FilesystemObject *fo);
+	void execute();
+	string getOperationName();
+	
+};
+
+#endif  //_FILESYSTEMOPERATION_H
---
+++ Source Files/FilesystemOperation.cpp
@@ -0,0 +1,35 @@
+//
+//
+//  @ Project : FileSystem
+//  @ File Name : FilesystemOperation.cpp
+//  @ Date : 9/16/2014
+//  @ Author : Stefan Djordjevic
+//
+//
+
+
+#include "../Header Files/FilesystemOperation.h"
+#include "../Header Files/FilesystemObject.h"
+#include <string>
+using namespace std;
+
+FilesystemOperation::FilesystemOperation(string fOpName, FilesystemObject *fo):objParameter(fo)
+{
+	opName = fOpName;
+}
+
+bool FilesystemOperation::checkPermissions() 
+{
+	return objParameter->checkAccess(opName);
+}
+
+void FilesystemOperation::execute() //Throws UnauthorizedAccessException, OperationFailedException
+{
+	if(checkPermissions()) performOperation();
+}
+
+string FilesystemOperation::getOperationName()
+{
+	return opName;
+}
+
