088f208b3676effec8f4758e7296d01137eb3b34
stefankv93 <stefankv93@gmail.com>
2014-09-16 16:40:27 +0200

    Added abstract class Filesystem Visitor

---
+++ Header Files/FilesystemVisitor.h
@@ -0,0 +1,24 @@
+//
+//
+//  @ Project : FileSystem
+//  @ File Name : FilesystemVisitor.h
+//  @ Date : 9/16/2014
+//  @ Author : Stefan Djordjevic
+//
+//
+
+
+#ifndef _FILESYSTEMVISITOR_H
+#define _FILESYSTEMVISITOR_H
+
+class File;
+class Folder;
+
+class FilesystemVisitor
+{
+public:
+	virtual void visitFile(File f) = 0;
+	virtual void visitFolder(Folder f) = 0;
+};
+
+#endif  //_FILESYSTEMVISITOR_H
